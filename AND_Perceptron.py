5ce93bc856087739e5c64c817aefc5f1e02ffdbb
RakhiGarhwal <garwalrakhi@gmail.com>
2026-02-02 13:49:03 +0530

    first commit

---
+++ AND_Perceptron.py
@@ -0,0 +1,8 @@
+def Perceptron(x1,x2):
+    w1,w2=1,1
+    b=-1.5
+    z=w1*x1+w2*x2+b
+    return 1 if z>= 0 else 0
+input=[(0,1),(0,0),(1,0),(1,1)]
+for x in input:
+    print(x,"->",Perceptron(x[0],x[1]))
